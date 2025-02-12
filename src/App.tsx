8e540d859e34c4fdef3ae964d4a18ce96749efda
hoangdev <phuchoang1232000@gmail.com>
2025-02-12 11:52:11 +0700

    update code

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { Github } from "lucide-react";
+import "./App.css";
+import { KanbanBoard } from "./components/KanbanBoard";
+import { ThemeToggle } from "./components/ThemeToggle";
+import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
+
+const FooterLink = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Button
+      variant="link"
+      asChild
+      className="scroll-m-20 text-xl font-semibold tracking-tight"
+    >
+      {children}
+    </Button>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="min-h-screen flex flex-col">
+          <header className="flex justify-between w-full flex-row p-4">
+            <Button variant="link" asChild className="text-primary h-8 w-8 p-0">
+              <a href="https://github.com/jaycenguyendev">
+                <Github className="fill-current h-full w-full" />
+              </a>
+            </Button>
+            <Button
+              variant="link"
+              asChild
+              className="text-primary h-16 w-16"
+            ></Button>
+            <ThemeToggle />
+          </header>
+          <main className="mx-4 flex flex-col gap-6">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              Drag and Drop Kanban Board
+            </h1>
+            <KanbanBoard />
+          </main>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
